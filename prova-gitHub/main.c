23768a76ba6003ba35065c6616b54f4bc54ff96c
Edoardo Conte <edoardo@mbp-di-edoardo.lan>
2017-03-25 12:01:25 +0100

    Initial Commit

---
+++ prova-gitHub/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  prova-gitHub
+//
+//  Created by Edoardo Conte on 25/03/17.
+//  Copyright © 2017 Edoardo Conte. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
